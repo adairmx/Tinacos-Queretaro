a73429cae72d6e43bcdc965d1fb203284984bc0e
adairmx <adairmx2013@gmail.com>
2026-03-12 22:51:40 -0600

    fix(ui): add ScrollToTop component to reset scroll on navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import ScrollToTop from "@/components/ScrollToTop";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Blog from "@/pages/Blog";
@@ -12,6 +13,7 @@ import AdminBlog from "@/pages/AdminBlog";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
